ab4e5b71785a2772bc6d72856805ed283dfdeef7
ameliamaria <ameliamaria.maciorowska@gmail.com>
2020-04-09 20:54:17 +0200

    add corrections

--- adopt_dog.py
+++ adopt_dog.py
@@ -1,13 +1,12 @@
 from dog_db_manager import DogDBManagerMock
 
+
 def adopt_dog():
     """ Take the id of the dog from the user (deal with the incorrect id), change the
     status of the correlating dog to 'adopted', save it and display it's details. """
-    adoptowany = int(input("Podaj nr ID, aby zgłosić adopcję pjemska: " ))
-    dog = DogDBManagerMock.get_dog(adoptowany)
+    adoption = int(input("Podaj nr ID, aby zgłosić adopcję pjemska: "))
+    dog = DogDBManagerMock.get_dog(adoption)
     dog.status = "adopted"
     dog.save()
     print(dog.name, "adoptowanyy!!!!!!")
     print(f"{dog.id}. {dog.name}, {dog.status}")
-
-
