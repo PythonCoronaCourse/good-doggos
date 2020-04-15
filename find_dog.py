a9d9772e6b81265536bbac8658b40a29ac25d552
Piotr <piotr.kaczynskii@gmail.com>
2020-04-16 01:09:49 +0200

    jestem spokojny

--- find_dog.py
+++ find_dog.py
@@ -6,17 +6,17 @@ def find_dog():
     """ Take the id or the name of the dog from the user, find the dog in the database and
     print the dog's details or info about absence of given dog in the database. If the name
     is given and there's more than one dog with that name, print info about all of them. """
-    dogs = DogDBManagerMock.get_dogs()
     id_name = int(input("Wpisz 0 - ID, 1 - imię: "))
     if id_name == 0:
         your_dogo = int(input("Podaj id pieska: "))
         dog = DogDBManagerMock.get_dog(your_dogo)
-        for dog in dogs:
-            if dog.id == your_dogo:
-                print(f"{dog.id}. {dog.name}, {dog.status}")
+        if DogDBManagerMock.get_dog(your_dogo):
+            print(f"{dog.id}. {dog.name}, {dog.status}")
+        else:
+            print("Nie ma takiego pieska")
     elif id_name == 1:
         your_dogo = input("Podaj imię pieska: ")
-        dog = DogDBManagerMock.get_dog(your_dogo)
+        dogs = DogDBManagerMock.get_dogs()
         for dog in dogs:
             if dog.name == your_dogo:
                 print(f"{dog.id}. {dog.name}, {dog.status}")
