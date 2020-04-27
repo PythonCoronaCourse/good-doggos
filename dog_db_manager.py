e00a6ae3f69cd8ee737664ecfb9174d07f030878
Py-Za <holytheship@gmail.com>
2020-04-27 22:04:48 +0200

    jezu nie radze sobie

--- dog_db_manager.py
+++ dog_db_manager.py
@@ -1,44 +1,63 @@
+# It's the most vital part of the system. It saves and loads the dogs from the "database", which is the dogs.txt file.
+#
+# It implements three static methods:
+#
+# save_dog(dog : Dog)
+# this method should load all the dogs from the dogs.txt file into a list, check if the dog with that id already exists, if it does replace it or append a new dog to that list and save it to the file again. If everything went okay True should be returned.
+#
+# get_dog(id : int)
+# this method should return the Dog that has provided id, or None, if such dog doesn't exist.
+#
+# get_dogs()
+# this method should return a list of all the dogs in the database.
+
+
 class Dog:
     dog_counter = 0
 
-    def __init__(self, name, year_of_birth, status):
+    def __init__(self, name, year_of_birth, status, id=None):
         self.name = name
         self.year_of_birth = year_of_birth
         self.status = status
-        self.id = Dog.dog_counter
-        Dog.dog_counter += 1
+        if id:
+            self.id = id
+        else:
+            self.id = Dog.dog_counter
+            Dog.dog_counter += 1
 
     def save(self):
-        DogDBManagerMock.save_dog(self)
+        DogDBManager.save_dog(self)
 
 
 class DogDBManager:
-    @staticmethod
-    def save_dog(dog):
-        """ Add the dog object to the database. """
-        pass
 
     @staticmethod
-    def get_dog(id):
-        """ Acquire the dog with the given ID from the database. """
-        pass
+    def __read_db():
+        return open("dogs.txt", "r")
 
     @staticmethod
-    def get_dogs():
-        """ Return all the dogs from the database as a list. """
-        pass
+    def __append_db():
+        return open("dogs.txt", "a")
 
+    @classmethod
+    def save_dog(cls, dog): # dodaje nowego psa
+        cls.__append_db().write("\n" + f"{dog.name},{dog.year_of_birth},{dog.status}")
 
-class DogDBManagerMock:
+    @classmethod
+    def get_dog(cls, id): # wyciaga z bazy pieska za pomoca jego id; id to nr linijki w DB
+        line = cls.__read_db().readlines()[id - 1]
+        return cls._serialize_dog(line)
+
+    @classmethod
+    def get_dogs(cls):
+        dogs = []
+        for line in cls.__read_db().readlines():
+            dogs.append(cls._serialize_dog(line))
+        return dogs
+
     @staticmethod
-    def save_dog(dog):
-        return True
+    def _serialize_dog(dog_line):
+        params = dog_line.split(",")
+        return Dog(*params) # *params to to samo co params[0], params[1], params[2]
 
-    @staticmethod
-    def get_dog(id):
-        return Dog("Psiulek", 2010, "for adoption")
 
-    @staticmethod
-    def get_dogs():
-        """ Return all the dogs from the database as a list. """
-        return [Dog("Tali", 2009, "adopted"), Dog("Baksiu", 2010, "for adoption")]
